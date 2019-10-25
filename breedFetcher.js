813e0386c67d6a70f0014ef2ce7c9c28fb6679cd
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-25 01:44:28 +0000

    cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,26 @@
-const input = process.argv.slice(2);
 const request = require('request');
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${input}`;
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data['status'] === 404) {
-    console.log("NO!!! such a Link Please Check Out");
-    process.exit();
-  } if (data[0] === undefined) {
-    console.log('No!!! such a Breed');
-  } else {
-    console.log(data[0]['description']);
-  }
-});
+const url = `https://api.thecatapi.com/v1/breeds/search?q=`;
+const fetchBreedDescription = function(breedName, callback) {
+  request(url + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data['status'] === 404) {
+      callback(error);
+      process.exit();
+    } if (data[0] === undefined) {
+      callback('No!!! such a Breed' , null );
+    } else {
+      callback(null , data[0].description);
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
+
+
+
+
+
+
+
 
 
 
